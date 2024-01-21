8425452fe205e623fc7f15d6545b629cf9fe68c7
SYAMRAJ <syamrajradhakrishnan@gmail.com>
2024-01-21 13:23:48 +0530

    started first upload

---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+ 
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3001, () => console.log("SERVER STARTED!"));
